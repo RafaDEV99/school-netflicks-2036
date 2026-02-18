1b681c3adf45edf593db6793546906e36ece6c03
RafaDEV99 <lionxd556@gmail.com>
2026-02-18 00:20:42 +0000

    Modifed upper part of the web

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import { Button } from './components/ui/button'
 import "./index.css"
 import { ButtonGroup } from './components/ui/button-group'
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallback,
+  AvatarGroup,
+  AvatarGroupCount,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 function App() {
   const [count, setCount] = useState(10)
@@ -10,13 +18,17 @@ function App() {
 
   return (
     <>
-    <div className='bg-secondary p-6'>
-      <p>epic!</p>
+    <div className='bg-secondary p-6 flex justify-between items-center'>
+      <h1 className='text-2xl'>Epic!</h1>
+      <Avatar>
+        <AvatarImage src='src/assets/react.svg' alt="@RafaDEV99"/>
+        <AvatarFallback>&gt;:3</AvatarFallback>
+      </Avatar>
     </div>
     <div className='grid gap-7 p-7 items-center justify-items-center'>
         <h1 className='font-bold text-4xl'><a className='hover:animate-pulse text-primary underline underline-offset-2'>Netflicks</a> 2026</h1>
         <h2 className={`${classUnder} text-1xl`}>Mi vida entre códigos</h2>
-        <p className="mask-type-alpha">
+        <p>
           "Lorem ipsum dolor sit amet, 
           consectetur adipiscing elit, 
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
