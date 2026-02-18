6dc7743e98035f8207a80db3025582f0f857a8bf
RafaDEV99 <lionxd556@gmail.com>
2026-02-18 08:21:00 +0000

    Update app.tsx in src folder

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,17 @@ function App() {
   return (
     <>
     <div className='bg-secondary p-6 flex justify-between items-center'>
-      <h1 className='text-2xl'>Epic!</h1>
+    <h1 className='font-bold text-4xl hover:animate-pulse text-primary underline underline-offset-2'>Epic!</h1>
       <Avatar>
         <AvatarImage src='src/assets/react.svg' alt="@RafaDEV99"/>
         <AvatarFallback>&gt;:3</AvatarFallback>
       </Avatar>
     </div>
     <div className='grid gap-7 p-7 items-center justify-items-center'>
-        <h1 className='font-bold text-4xl'><a className='hover:animate-pulse text-primary underline underline-offset-2'>Netflicks</a> 2026</h1>
-        <h2 className={`${classUnder} text-1xl`}>Mi vida entre códigos</h2>
-        <p>
+      <h1 className='font-bold text-4xl'><a className='hoanimate-pulse text-primary underline underline-offset-2'>Netflicks</a> 2026</h1>
+
+      <h2 className={`${classUnder} text-1xl`}>Mi vida entre códigos</h2>
+      <p>
           "Lorem ipsum dolor sit amet, 
           consectetur adipiscing elit, 
           sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -39,13 +40,13 @@ function App() {
           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
           Excepteur sint occaecat cupidatat non proident, 
           sunt in culpa qui officia deserunt mollit anim id est laborum."
-        </p>
-        <img src="src/assets/Poster_Rafa.png" className='h-75'></img>
-        <Button className='bg-auto'>Ver trailer</Button>
-        <ButtonGroup className='gap-3'>
-          <Button onClick={() => setCount(count + 1)}>Incrementar</Button>
-          <Button onClick={() => setCount(count - 1)}>Decrementar</Button>
-        </ButtonGroup>
+      </p>
+      <img src="src/assets/Poster_Rafa.png" className='h-75'></img>
+      <Button className='bg-auto'>Ver trailer</Button>
+      <ButtonGroup className='gap-3'>
+        <Button onClick={() => setCount(count + 1)}>Incrementar</Button>
+        <Button onClick={() => setCount(count - 1)}>Decrementar</Button>
+      </ButtonGroup>
     </div>
     </>
   )
