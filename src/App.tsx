37e272441c9022770e43762ea8ef626495e72a7e
RafaDEV99 <lionxd556@gmail.com>
2026-02-19 09:33:18 +0000

    Updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,14 +33,8 @@ function App() {
 
       <h2 className={`${classUnder} text-1xl`}>Mi vida entre códigos</h2>
       <p>
-          "Lorem ipsum dolor sit amet, 
-          consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-          Ut enim ad minim veniam, 
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, 
-          sunt in culpa qui officia deserunt mollit anim id est laborum."
+        "El exelente documental sobre Rafael Tangyan ha salido!
+        Descubre sobre como el futuro le dío la verdad a Rafael"
       </p>
       <img src="src/assets/Poster_Rafa.png" className='h-75'></img>
       <Button className='bg-auto'>Ver trailer</Button>
