439d19148b8e8cbc041404b957e3374e4fe5a8fb
RafaDEV99 <lionxd556@gmail.com>
2026-02-27 15:47:03 +0000

    Updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tailwindcssMotion from "tailwindcss-motion";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,6 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: [tailwindcssMotion],
   },
 ])
